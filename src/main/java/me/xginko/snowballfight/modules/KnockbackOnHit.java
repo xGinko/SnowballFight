df30c0fc8f39093cdfff04ff3b02c327a3789520
xGinko <rachel.audrey@mail.de>
2023-11-20 12:57:31 +0100

    improve knockback module

--- src/main/java/me/xginko/snowballfight/modules/KnockbackOnHit.java
+++ src/main/java/me/xginko/snowballfight/modules/KnockbackOnHit.java
@@ -22,6 +22,7 @@ public class KnockbackOnHit implements SnowballModule, Listener {
     private final ServerImplementation scheduler;
     private final HashSet<EntityType> configuredTypes = new HashSet<>();
     private final Vector vectorModifier;
+    private final double multiplier;
     private final boolean isFolia, modifyVector, onlyForSpecificEntities, asBlacklist;
 
     protected KnockbackOnHit() {
@@ -31,10 +32,12 @@ public class KnockbackOnHit implements SnowballModule, Listener {
         this.scheduler = isFolia ? foliaLib.getImpl() : null;
         SnowballConfig config = SnowballFight.getConfiguration();
         config.master().addComment("settings.knockback", "Modify knockback values on snowball hit.");
-        this.modifyVector = config.getBoolean("settings.knockback.vector-modifier.enable", false);
+        this.multiplier = config.getDouble("settings.knockback.multiplier", 1.2,
+                "The multiplier for the knockback of the snowball.");
+        this.modifyVector = config.getBoolean("settings.knockback.vector-modifier.enable", true);
         this.vectorModifier = new Vector(
                 config.getDouble("settings.knockback.vector-modifier.x", 0.0),
-                config.getDouble("settings.knockback.vector-modifier.y", 3.0),
+                config.getDouble("settings.knockback.vector-modifier.y", 0.6),
                 config.getDouble("settings.knockback.vector-modifier.z", 0.0)
         );
         this.onlyForSpecificEntities = config.getBoolean("settings.knockback.only-for-specific-entities", false,
@@ -80,9 +83,9 @@ public class KnockbackOnHit implements SnowballModule, Listener {
         if (hitEntity == null) return;
 
         if (isFolia) {
-            scheduler.runAtEntity(hitEntity, knockback -> hitEntity.setVelocity(modifyVector ? snowball.getVelocity().add(vectorModifier) : snowball.getVelocity()));
+            scheduler.runAtEntity(hitEntity, knockback -> hitEntity.setVelocity(modifyVector ? snowball.getVelocity().multiply(multiplier).add(vectorModifier) : snowball.getVelocity().multiply(multiplier)));
         } else {
-            hitEntity.setVelocity(modifyVector ? snowball.getVelocity().add(vectorModifier) : snowball.getVelocity());
+            hitEntity.setVelocity(modifyVector ? snowball.getVelocity().multiply(multiplier).add(vectorModifier) : snowball.getVelocity().multiply(multiplier));
         }
     }
 }
